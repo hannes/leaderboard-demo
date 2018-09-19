44b3cfa4e7b673bc594047f8612b8a947c75aea4
admin <admin@localhost.localdomain>
2018-09-19 16:39:25 +0200

    add example benchmarking file

---
+++ spark/bench.scala
@@ -0,0 +1,14 @@
+:load reorg_cruncher.scala 
+
+val path = "/opt/lsde/dataset-sf100"
+reorg(path)
+cruncher(path,2811,1990,5183,1749,409,509)
+cruncher(path,11254,143,2807,2799,430,530)
+cruncher(path,7517,568,2820,6945,730,829)
+cruncher(path,782,2008,1022,8,827,926)
+cruncher(path,206,2837,808,7509,423,523)
+cruncher(path,1998,2812,9474,2803,219,321)
+cruncher(path,1987,12002,1180,2870,1126,1226)
+cruncher(path,141,9141,6704,2800,910,1010)
+cruncher(path,565,1989,1023,7380,924,1024)
+cruncher(path,1183,2059,1179,1985,806,905)
